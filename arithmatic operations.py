fa14094eb438eca50c8393c3ca52d63d5befc265
Malkeet <55949501+malkeetsingh0130@users.noreply.github.com>
2025-05-19 00:07:34 +0530

    Add files via upload

---
+++ arithmatic operations.py
@@ -0,0 +1,16 @@
+print(10+3)
+print(10-3)
+print(10/3) #return float
+print(10//3) #return integer value
+print(10%3) #returns reaminder
+print(10**3) #return power of number
+
+
+#for incrementing numbers
+x=10
+x=x+3
+print(x)
+#augmented assignment opertor. This will do the same this as previous output but is a shorter way of writing
+y=10
+y+=3
+print(y)
