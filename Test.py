3d6c163c5d68b09bbed6426916d9e05ee86fb861
malkeetsingh0130 <55949501+malkeetsingh0130@users.noreply.github.com>
2025-05-18 21:53:41 +0530

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,4 @@
+print ("Hello World")
+print ("||||||")
+print (" ||||")
+print ("Malkeet " *10)
---
+++ get input.py
@@ -0,0 +1,3 @@
+name=input('What is your name? ')
+fav_color=input("What's your Favorite Color? ")
+print (name+ ' likes '+fav_color)
