8081915bbcd39b655b31e8569f468a0aa80181c2
Malkeet <55949501+malkeetsingh0130@users.noreply.github.com>
2025-05-23 00:01:02 +0530

    Add files via upload

---
+++ operator precedence.py
@@ -0,0 +1,12 @@
+# Order of execution is a below-
+# parenthesis
+# exponentiation
+# multiplication or division
+# addition or substraction
+
+x=10+3*2
+print(x)
+y=10+3*2**2
+print(y)
+z=(2+3)*10-3
+print(z)
