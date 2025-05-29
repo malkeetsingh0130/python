d59f2822f3458a035d8943437b95873ac8fd0e51
malkeetsingh0130 <malkeetsingh0130@gmail.com>
2025-05-29 23:24:30 +0530

    loopingstringupdate

--- loopingthroughstrings.py
+++ loopingthroughstrings.py
@@ -1,6 +1,8 @@
 fruit=input("Enter the fruit you want to index: ")
 index=0
 while index<len(fruit):
-    letter=fruit[index]
+    #variable index is being used as an iterator whole value is increasing with every loop.
+    #[] brackets are used for indexing.are used in Python to access elements of sequences like lists and strings.
+    letter=fruit[index] 
     print(index,letter)
     index=index+1
