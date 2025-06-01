1965c52c6b4559353ac57822f662dc7c7ee8fa2c
malkeetsingh0130 <malkeetsingh0130@gmail.com>
2025-06-01 20:36:10 +0530

    strings

---
+++ Playing with strings/string methods.py
@@ -0,0 +1,8 @@
+course='python for Beginners'
+print(len(course))
+print(course.upper())
+print(course.capitalize())
+print(course.find('o'))
+print(course.replace('Beginners','Absolute Beginners'))
+print('python'in course)
+print('test'in course)
