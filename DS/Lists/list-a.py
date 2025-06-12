2669dd8e558b7475cb4980c05dc474383745d0f0
malkeetsingh0130 <malkeetsingh0130@gmail.com>
2025-06-13 00:05:58 +0530

    update1

--- DS/Lists/list-a.py
+++ DS/Lists/list-a.py
@@ -4,3 +4,12 @@ for i in ['John', 'Doe', 'Ruby', 'Rails']:
 #lists can be sliced
 t=[9,14,20,21,30,40]    
 print(t[1:5])
+
+#appending a list
+value=list()
+while True:
+    inp=input('Enter a value: ')
+    if inp== 'done' : break
+    value.append(inp)
+print(value)
+    
