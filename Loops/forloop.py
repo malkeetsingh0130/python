a4052a479cc43e090d295417365c20a94b228f38
malkeetsingh0130 <malkeetsingh0130@gmail.com>
2025-08-11 21:47:07 +0530

    for_loop

---
+++ Loops/forloop.py
@@ -0,0 +1,7 @@
+fruits=["Apple","Orange","Pineapple","Mango","Guava","Strawberry"]
+
+for fruit in fruits:
+    print("Would you like a {} ?".format(fruit))
+
+for number in range(1,12):
+    print("Number is {}".format(number))
