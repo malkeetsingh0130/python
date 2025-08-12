d9cdace7aadf298c564e5b4a5cd9f11f091598d8
malkeetsingh0130 <malkeetsingh0130@gmail.com>
2025-08-12 22:41:02 +0530

    checking members

---
+++ DS/Lists/checking_membership.py
@@ -0,0 +1,9 @@
+# Checking membership means if the item is present in the list
+fruits=["orange", "pineapple", "banana", "mango"]
+years=[1990, 1998, 2000, 2001, 29.97, 7826]
+
+# in will return boolean is the item is present in the list
+print("apple" in fruits)
+
+# count function can be used to return the count of it
+print(fruits.count("orange"))
