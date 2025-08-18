1a9ead295e8fee0bae55b556673d26cb8b4a311a
malkeetsingh0130 <malkeetsingh0130@gmail.com>
2025-08-18 19:04:06 +0530

    Practive level 1

---
+++ Practice/Level 1 – Basics & Data Types/simple_math.py
@@ -0,0 +1,7 @@
+number1=int(input("Enter the first number- "))
+number2=int(input("Enter the second number- "))
+
+print("Sum of both the number is-",number1+number2)
+print("Difference of both the numbers is-",number1-number2)
+print("Product of both the numbers is-",number1*number2)
+print("Division of both the numbers give-",number1/number2)
---
+++ Practice/Level 1 – Basics & Data Types/swap_variables.py
@@ -0,0 +1,9 @@
+variable1=int(input("Enter the value of first variable - "))
+variable2=int(input("Enter the value of second variable- "))
+
+variable1=variable1+variable2
+variable2=variable1-variable2
+variable1=variable1-variable2
+
+print("Current value for variable 1 is-",variable1)
+print("Current value for variable 2 is-",variable2)
